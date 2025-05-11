901d9142558b569da5ada8e48a6cf960730ce304
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-05-11 07:16:49 +0530

    React from scratch

---
+++ App.js
@@ -0,0 +1,13 @@
+// const heading = React.createElement("h1",{ id:"heading"},"Hello World from React!");
+//  const root = ReactDOM.createRoot(document.getElementById("root"));
+//  console.log("what is heading ?", heading);
+
+const parent = React.createElement( "div",{id:"parent"}, 
+   [ React.createElement("div", {id:"child1"}, 
+        [React.createElement("h1",{}, "I am h1 tag"),React.createElement("h2",{}, "I am h2 tag")]),
+        React.createElement("div", {id:"child2"}, 
+        [React.createElement("h1",{}, "I am h1 tag"),React.createElement("h2",{}, "I am h2 tag")])
+    
+    ] );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(parent);
