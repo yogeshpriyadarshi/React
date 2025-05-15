774179274006034d228f82820b942feb522897f6
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-05-15 14:36:25 +0530

    Add React tp Project

---
+++ dist/React.6bd02f5a.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, " New Project");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=React.6bd02f5a.js.map
