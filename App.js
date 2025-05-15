1e12cb3240b26bd2488705bd75edd6ed1b70cac9
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-05-15 14:22:34 +0530

    new Project

--- App.js
+++ App.js
@@ -1,13 +1,3 @@
-// const heading = React.createElement("h1",{ id:"heading"},"Hello World from React!");
-//  const root = ReactDOM.createRoot(document.getElementById("root"));
-//  console.log("what is heading ?", heading);
-
-const parent = React.createElement( "div",{id:"parent"}, 
-   [ React.createElement("div", {id:"child1"}, 
-        [React.createElement("h1",{}, "I am h1 tag"),React.createElement("h2",{}, "I am h2 tag")]),
-        React.createElement("div", {id:"child2"}, 
-        [React.createElement("h1",{}, "I am h1 tag"),React.createElement("h2",{}, "I am h2 tag")])
-    
-    ] );
+const heading  = React.createElement("h1",{id:"heading"}, " New Project");
 const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(parent);
+root.render(heading);
